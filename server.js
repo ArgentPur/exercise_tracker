9ecac22e3d5bd67e833042ff7c1adef88cd257cb
Zachary Lynn <zalynn25@hotmail.com>
2020-12-19 00:23:29 -0500

    serverUpdate

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
+const app = express();
 const port = process.env.MONGOOSE || 3000
+
+require(".routes/api_routes")(app);
+require(".routes/html_routes")(app);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost",)
+
+app.listen(PORT, function() {
+    console.log(`Listening on port: ${PORT}`);
+});
