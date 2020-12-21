be9b31990068d3802ec45d393f3f5b41f20ea9e8
Zachary Lynn <zalynn25@hotmail.com>
2020-12-21 00:22:53 -0500

    update

--- routes/html_routes.js
+++ routes/html_routes.js
@@ -1,2 +1,14 @@
 const path = require("path");
 
+module.exports = function(app) {
+    app.get("/", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+    app.get("/exercise", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercice.html"));
+    });
+    app.get("/stats", function(req, res) {
+        res.sendFile(path.join(__dirname, "../public/stats.html"));
+    });
+}
+
