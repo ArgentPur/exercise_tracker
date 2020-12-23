c080dea97afd5ba09db3236f70d2dca346dec20e
Zachary Lynn <zalynn25@hotmail.com>
2020-12-22 23:48:11 -0500

    serverUpdate

--- server.js
+++ server.js
@@ -4,11 +4,21 @@ let db = require("./models")
 
 const app = express();
 const PORT = process.env.MONGOOSE || 3000
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
 
 app.use(require(".routes/api_routes")(app));
 app.use(require(".routes/html_routes")(app));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost",)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
 
 app.listen(PORT, function() {
     console.log(`Listening on port: ${PORT}`);
